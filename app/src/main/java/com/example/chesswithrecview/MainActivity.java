8d7671b5298c44ae604172e40a1a2189e89aac11
saraawaad2 <sara.schoo22.25@gmail.com>
2024-10-09 16:24:48 +0300

    fdgf

--- app/src/main/java/com/example/chesswithrecview/MainActivity.java
+++ app/src/main/java/com/example/chesswithrecview/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.chesswithrecview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,7 +15,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements StoneAdapter.ItemSelected {
     private Button button;
-    private TextView textView;
     private RecyclerView recyclerView;
     private StoneAdapter myAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -42,18 +38,17 @@ public class MainActivity extends AppCompatActivity implements StoneAdapter.Item
 
     private void initImgArray() {
         images=new int[6];
-        images[0]=R.drawable.king_piece;
-        images[1]=R.drawable.queen_piece;
-        images[2]=R.drawable.bishop_piece;
-        images[3]=R.drawable.knight_piece;
-        images[4]=R.drawable.rook_piece;
-        images[5]=R.drawable.pawn_piece;
+        images[0]=R.drawable.d;
+        images[1]=R.drawable.y;
+        images[2]=R.drawable.t;
+        images[3]=R.drawable.zz;
+        images[4]=R.drawable.z;
+        images[5]=R.drawable.g;
     }
 
     private void initViews() {
         button=findViewById(R.id.btnShowList);
         recyclerView=findViewById(R.id.list);
-        textView=findViewById(R.id.txtDetails);
         recyclerView.setHasFixedSize(true);
         layoutManager=new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -75,7 +70,24 @@ public class MainActivity extends AppCompatActivity implements StoneAdapter.Item
 
     @Override
     public void onItemClicked(int Index) {
-        String Details[]=getResources().getStringArray(R.array.details);
-        textView.setText(Details[Index]);
+
+    if(Index==0){
+        Intent intent = new Intent(this, Daimond.class);
+        startActivity(intent);}
+    if(Index==1){
+        Intent intent = new Intent(this, Yaqot.class);
+        startActivity(intent);}
+    if(Index==2){
+        Intent intent = new Intent(this, Tobaz.class);
+        startActivity(intent);}
+    if(Index==3){
+        Intent intent = new Intent(this, Zafer.class);
+        startActivity(intent);}
+    if(Index==4){
+        Intent intent = new Intent(this, Zomorod.class);
+        startActivity(intent);}
+    if(Index==5){
+        Intent intent = new Intent(this, Gem.class);
+        startActivity(intent);}
     }
-}
+    }
